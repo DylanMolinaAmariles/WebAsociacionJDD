6f67224aba139494a05452cc9b3544819191e6d4
DylanMolinaAmariles <dylanmolina1973@gmail.com>
2024-02-06 20:23:41 +0100

    modificado el registro y casi terminada la pagina de informacion

---
+++ QuienSomos.js
@@ -0,0 +1,18 @@
+function toggleMenu() {
+    var nav = document.getElementById('nav');
+    nav.style.display = (nav.style.display === 'block') ? 'none' : 'block';
+}
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
